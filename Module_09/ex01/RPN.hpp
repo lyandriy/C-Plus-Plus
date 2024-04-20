6c08c06ad01bf1e996882507a5964d6c2cece2cf
Lyudmyla Andriychyk Fetsenets <lyandriy@c1r6s5.42madrid.com>
2024-04-20 16:51:16 +0200

    cpp09_ex02_ok

--- Module_09/ex01/RPN.hpp
+++ Module_09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: lyandriy <lyandriy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 19:42:53 by lyandriy          #+#    #+#             */
-/*   Updated: 2024/04/18 19:49:10 by lyandriy         ###   ########.fr       */
+/*   Updated: 2024/04/20 15:26:56 by lyandriy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,17 @@
 
 #include <stack>
 #include <iostream>
+#include <queue>
+#include <limits>
+#include <cfloat>
 
 int	rpn(char **argv);
+
+class	error : public std::exception
+{
+	public:
+		const char* what() const throw()
+		{return ("Error");}
+};
 
 #endif
--- Module_09/ex01/main.cpp
+++ Module_09/ex01/main.cpp
@@ -5,49 +5,168 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lyandriy <lyandriy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/18 19:44:19 by lyandriy          #+#    #+#             */
-/*   Updated: 2024/04/18 20:09:17 by lyandriy         ###   ########.fr       */
+/*   Created: 2024/04/19 18:25:34 by lyandriy          #+#    #+#             */
+/*   Updated: 2024/04/20 16:47:23 by lyandriy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
-int	str_isdigit(char *argv)
+long int	make_digit(char *argv, int &i)
 {
-	for (int i = 0; argv[i]; i++)
+	std::string	s(argv);
+	long int	count = 0;
+
+	while (std::isdigit(argv[i]))
 	{
-		if (!std::isdigit(argv[i]))
-			return (0);
+		i++;
+		count++;
 	}
-	return (1);
+	count = std::atol(s.substr((i - count), count).c_str());
+	if (count < INT_MIN || count > INT_MAX)
+		throw error();
+	return (count);
 }
 
-int	rpn(char **argv)
+double	numb_size(char *argv)
 {
-	std::stack<int> numb;
+	std::queue<long int>	numb;
+	int	i = 0;
+	size_t	oper = 0;
 
-	for (int i = 1; argv[i]; i++)
+	while (argv[i])
 	{
-		if (str_isdigit(argv[i]))
-			numb.push(std::atoi(argv[i]));
-		if (argv[i] == "+" || argv[i] == "-" || argv[i] == "*" || argv[i] == "/")
-			
+		if (std::isdigit(argv[i]))
+			numb.push(make_digit(argv, i));
+		else if (argv[i] == '+' || argv[i] == '-' || argv[i] == '*' || argv[i] == '/')
+		{
+			i++;
+			oper++;
+		}
+		else if (argv[i] == ' ')
+			i++;
+		else////
+			throw error();
 	}
+	if (numb.size() == 1 && oper == 0)
+		return (numb.front());
+	if (numb.size() > 10 || (numb.size() - 1) != oper)
+		throw error();
 	return (0);
+}
+
+std::string	str_isdigit(char *argv, int &i)
+{
+	std::string	str;
+
+	while (std::isdigit(argv[i]))
+	{
+		str += argv[i];
+		i++;
+	}
+	if (argv[i] != ' ')
+		throw error();
+	while (argv[i] == ' ')
+		i++;
+	return (str);
+}
+
+void	str_isit(char *argv, int &i, std::stack<char> &oper)
+{
+	while ((argv[i] == '+' || argv[i] == '-' || argv[i] == '*' ||
+			argv[i] == '/') && argv[i] != '\0')
+	{
+		oper.push(argv[i]);
+		i++;
+		while (argv[i] == ' ')
+			i++;
+	}
+}
+
+void	operations(std::stack<char> &oper, std::queue<long int> &numb)
+{
+	long int	res_num;
+
+	if (numb.empty() || oper.empty())
+		throw error();
+	while (!numb.empty() && !oper.empty())
+	{
+		res_num = numb.front();
+		numb.pop();
+		if (oper.top() == '+')
+			numb.push(res_num + numb.front());
+		else if (oper.top() == '-')
+			numb.push(res_num - numb.front());
+		else if (oper.top() == '*')
+			numb.push(res_num * numb.front());
+		else if (oper.top() == '/')
+			numb.push(res_num / numb.front());
+		numb.pop();
+		oper.pop();
+	}
+}
+
+int	rpn(char *argv)
+{
+	std::stack<char>			oper;
+	std::queue<long int>		numb;
+	int i = 0;
+
+	if (numb_size(argv))
+		return (std::atoi(argv));
+	while (argv[i])
+	{
+		while (std::isdigit(argv[i]))
+			numb.push(std::atoi(str_isdigit(argv, i).c_str()));
+		if (argv[i] == '+' || argv[i] == '-' || argv[i] == '*' || argv[i] == '/')
+			str_isit(argv, i, oper);
+		/*else if (!std::isdigit(argv[i]) && argv[i] != '+' && argv[i] != '-' &&
+				argv[i] != '*' && argv[i] != '/' && argv[i] != ' ' && argv[i] != '\0' )
+			throw error();*/
+		operations(oper, numb);
+	}
+	return (numb.front());
 }
 
 int	main(int argc, char **argv)
 {
 	try
 	{
-		if (argc > 1)
-			std::cout << "result " << rpn(argv) << std::endl;
+		if (argc == 2 && argv[1][0] != '\0')
+		{
+			int result = rpn(argv[1]);
+			std::cout << result << std::endl;
+		}
 		else
-			throw std::exception();
+			throw error();
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << e.what() << std::endl;;
+		std::cout << e.what() << std::endl;;
 	}
-	
+	return (0);
 }
+
+
+/*int		numb = 0;
+	int		oper = 0;
+	long int	one_numb;
+
+	for (int i = 0; argv[i]; i++)
+	{
+		if (std::isdigit(argv[i]))
+			numb++;
+		if (argv[i] == '+' || argv[i] == '-' || argv[i] == '*' || argv[i] == '/')
+			oper++;
+	}
+	std::cout << numb << "\n";
+	if (numb == 1)
+	{
+		one_numb = std::atoi(argv);
+		if (one_numb < INT_MIN || one_numb > INT_MAX)
+			throw error();
+		return (one_numb);
+	}
+	if (numb > 10 || (numb - 1) != oper)
+		throw error();
+	return (0);*/
